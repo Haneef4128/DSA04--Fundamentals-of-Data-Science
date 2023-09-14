ae1cbe73cc70ebd064163b02dd8220513b68db93
Haneef4128 <114045201+Haneef4128@users.noreply.github.com>
2023-09-14 13:41:12 +0530

    exp36

---
+++ exp36.py
@@ -0,0 +1,13 @@
+import pandas as pd
+stock_data = pd.read_csv("finance.csv")
+stock_data['PriceChange'] = stock_data['ClosingPrice'].diff()
+mean_change = stock_data['PriceChange'].mean()
+std = stock_data['PriceChange'].std()
+print("Stock Price Variability Analysis")
+print(f"Mean Daily Price Change: {mean_change:.2f}")
+print(f"Standard Deviation of Daily Price Changes: {std:.2f}")
+positive_changes = stock_data[stock_data['PriceChange'] > 0]['PriceChange'].count()
+negative_changes = stock_data[stock_data['PriceChange'] < 0]['PriceChange'].count()
+print("\nStock Movement Direction")
+print(f"Days with Positive Price Change: {positive_changes}")
+print(f"Days with Negative Price Change: {negative_changes}")
